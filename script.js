3d2768127b54f095863af705be5d50ae5c2d6f3e
rohitlakhotia11-lab <rohitlakhotia11@gmail.com>
2026-08-28 13:14:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
   BOOKING_LINK: your Google Calendar appointment schedule
 */
 
-const PAYMENT_LINK = "https://rzp.io/rzp/zi6HNacH";
+const PAYMENT_LINK = "https://rzp.io/rzp/7EMgpko";
 const BOOKING_LINK = "https://calendar.app.google/zAUHqCz8n5NpSpM67";
 
 document.addEventListener("DOMContentLoaded", () => {
